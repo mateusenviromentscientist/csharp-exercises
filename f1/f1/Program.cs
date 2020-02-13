cb6bc669febbc52be476465a5eb81e17de1ad71d
mateus <mateus.seducsobral@gmail.com>
2020-02-12 21:48:47 -0300

    add f1

---
+++ f1/f1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace f1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string val = "Whatever we want";
+            string val2 = "whatever";
+            Console.WriteLine(val);
+
+            Console.WriteLine(val.ToUpper());
+
+            Console.WriteLine(val);
+
+            Console.WriteLine(val.ToLower().Contains("whatever"));
+
+            Console.WriteLine(val.LastIndexOf('t'));
+            Console.WriteLine(val.Length);
+
+            Console.WriteLine(val.Replace('w', 'j'));
+
+            Console.WriteLine(val.Remove(0,3));
+            Console.WriteLine(val.Remove(2, val.Length - 4));
+
+            Console.WriteLine(val.Substring(3,5));
+            Console.WriteLine(val.StartsWith("Wha"));
+            Console.WriteLine(val.EndsWith("t"));
+
+            string url = "         www. google.com/pedo/de/lara"        ;
+            string[] part = url.Split('/');
+            Console.WriteLine(part[0]);
+
+            Console.WriteLine(url);
+            Console.WriteLine(url.Trim());
+
+
+        }
+    }
+}
---
+++ functions/functions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace functions
+{
+    class Program
+    {
+        public static int squared(int x)
+        {
+            int y = x * x;
+            return y;
+        }
+
+        public static void printnumber(int number)
+        {
+            Console.WriteLine("number: " + number);       
+        }
+
+
+        public static Boolean isBigger(int first, int second)
+        {
+            Boolean temp = first > second;
+            return temp;
+
+        }
+        static void Main(string[] args)
+        {
+            int temp = 10;
+            int result = squared(temp);
+            printnumber(temp);
+            printnumber(result);
+            Console.WriteLine(isBigger(10, 20));
+            if(isBigger(20, 10)) ;
+            {
+                printnumber(result);
+            }
+        }
+    }
+}
