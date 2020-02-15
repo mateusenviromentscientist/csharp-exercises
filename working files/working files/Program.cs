3c8caec1d6023b98fdc9c4a8c7262a169ac0c6b3
mateus <mateus.seducsobral@gmail.com>
2020-02-14 22:05:21 -0300

    add last things about course

---
+++ working files/working files/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace working_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (File.Exists("settings/mytextfile.txt")) ;
+            
+                
+            
+            FileInfo info = new FileInfo("settings/mytextfile.txt");
+            Console.WriteLine(info.Directory);
+            Console.WriteLine(info.Extension);
+            Console.WriteLine(info.IsReadOnly);
+            Console.WriteLine(info.LastAccessTimeUtc);
+            Console.WriteLine(info.LastWriteTime);
+            Console.WriteLine(info.Length);
+
+             
+
+    
+        
+        
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
